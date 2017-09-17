a15a7080e71a931291a4a31437ea4ee53b3e9488
Ivanov Maxim <manwithdagger@yandex.ru>
2017-09-17 21:37:36 +0700

    fix

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.executeScript(tab.id, {code: "console.log('toggle on')"});
         chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
         chrome.tabs.insertCSS(tab.id, {file:"style.css"});
-        chrome.tabs.executeScript(tab.id, {file: script.js});
+        chrome.tabs.executeScript(tab.id, {file: "script.js"});
     }
     else{
         chrome.tabs.executeScript(tab.id, {code: "window.location.reload()"});
